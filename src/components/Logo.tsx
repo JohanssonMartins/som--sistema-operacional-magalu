1b007160c0922139b5e5bff394d7b1efee552b45
Johansson <johanssonmmg@gmail.com>
2026-03-12 15:41:53 -0300

    alteração realizada

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="flex items-center justify-center h-[64px] md:h-[72px] overflow-hidden">
       <img
-        src="C: \Users\joh_gomes\Desktop\som--sistema-operacional-magalu\static\Logo SOM - Completo - Azul.png"
+        src="/Logo SOM - Completo - Azul.png"
         alt="Sistema Operacional Magalog"
         className="h-full w-auto object-contain scale-[1.7] transform transition-transform duration-300 hover:scale-[1.8]"
       />
